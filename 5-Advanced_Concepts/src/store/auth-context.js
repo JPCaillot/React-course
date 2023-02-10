7ca7fec73f72349c54cf0cdbfae6ba9c93c7a712
JPCaillot <joaopcaillot@gmail.com>
2023-02-10 16:52:11 -0300

    Rules of Hooks, Context, ImperativeHandle

--- 5-Advanced_Concepts/src/store/auth-context.js
+++ 5-Advanced_Concepts/src/store/auth-context.js
@@ -1,7 +1,46 @@
-
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+  isLoggedIn: false,
+  onLogout: () => {}, //dummy function for IDE completion
+  onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    };
+  }, []); //dependencies will never change (they're empty) because we only want it to re-evaluate (run the function) once, when the page is started
+
+  
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
