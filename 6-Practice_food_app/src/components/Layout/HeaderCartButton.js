980d46465f5ea878111ab112d0ea68d6f2327b54
JPCaillot <joaopcaillot@gmail.com>
2023-02-14 15:00:41 -0300

    Finished food app

--- 6-Practice_food_app/src/components/Layout/HeaderCartButton.js
+++ 6-Practice_food_app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,48 @@
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
+import {useContext, useEffect, useState} from 'react';
+import CartContext from '../../store/cart-context';
 
-const HeaderCartButton = props => {
-    return <button className={classes.button}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>
-            3
-        </span>
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const {items} = cartCtx; //destructuring
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount; 
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return; //when the page is first loaded, it doesn't execute
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer)
+    }; //automatically a clearup function in useEffect, good practice 
+  }, [items]);
+  
+  return (
+    <button 
+      className={btnClasses} 
+      onClick={props.onClick}
+    >
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
+  );
 };
 
 export default HeaderCartButton;
