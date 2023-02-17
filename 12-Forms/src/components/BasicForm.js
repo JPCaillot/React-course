7158f3c69d19f26bcc0bdeebd9b02d5ed1c6ed5f
JPCaillot <joaopcaillot@gmail.com>
2023-02-17 12:04:35 -0300

    Finished new hook

--- 12-Forms/src/components/BasicForm.js
+++ 12-Forms/src/components/BasicForm.js
@@ -1,64 +1,110 @@
 import { useState } from "react";
 
+import useNew from "../hooks/use-new";
+
 const BasicForm = (props) => {
-  const [firstName, setFirstName] = useState("");
-  const [firstIsTouched, setFirstIsTouched] = useState(false);
+  const {
+    inputValue: firstName,
+    hasError: firstHasError,
+    inputIsValid: firstIsValid,
+    inputChangeHandler: firstChangeHandler,
+    inputBlurHandler: firstBlurHandler,
+    cleanupHandler: firstCleanHandler,
+  } = useNew(value => value.trim() !== '');
 
-  const firstNameIsValid = firstName.trim() !== "";
-  const firstHasError = (!firstNameIsValid && firstIsTouched);
-  
-  const formError = firstHasError; //new const for the form  // and the other fields
+  const {
+    inputValue: lastName,
+    hasError: lastHasError,
+    inputIsValid: lastIsValid,
+    inputChangeHandler: lastChangeHandler,
+    inputBlurHandler: lastBlurHandler,
+    cleanupHandler: lastCleanHandler,
+  } = useNew(value => value.trim() !== '');
 
-  const firstChangeHandler = (event) => {
-    setFirstName(event.target.value);
-  };
+  const {
+    inputValue: email,
+    hasError: emailHasError,
+    inputIsValid: emailIsValid,
+    inputChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    cleanupHandler: emailCleanHandler,
+  } = useNew(value => value.includes('@'));
 
-  const firstBlurHandler = (event) => {
-    setFirstIsTouched(true);
-  };
+  // const [firstName, setFirstName] = useState("");
+  // const [firstIsTouched, setFirstIsTouched] = useState(false);
+
+  // const firstNameIsValid = firstName.trim() !== "";
+  // const firstHasError = !firstNameIsValid && firstIsTouched;
+
+  let formIsValid = false; //new const for the form  // and the other fields
+
+  if (firstIsValid && lastIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  // const firstChangeHandler = (event) => {
+  //   setFirstName(event.target.value);
+  // };
+
+  // const firstBlurHandler = (event) => {
+  //   setFirstIsTouched(true);
+  // };
 
   const submitHandler = (event) => {
     event.preventDefault();
 
-    setFirstIsTouched(true);
-
-    if (formError) {
+    if (!formIsValid) {
       return;
     }
-
-    setFirstIsTouched(false);
-    setFirstName("");
+    
+    firstCleanHandler();
+    lastCleanHandler();
+    emailCleanHandler();
   };
 
-  const className = firstHasError
-    ? "form-control invalid"
-    : "form-control";
+  const firstClassName = firstHasError ? "form-control invalid" : "form-control";
+  const lastClassName = lastHasError ? "form-control invalid" : "form-control";
+  const emailClassName = emailHasError ? "form-control invalid" : "form-control";
 
   return (
     <form onSubmit={submitHandler}>
       <div className="control-group">
-        <div className={className}>
+        <div className={firstClassName}>
           <label htmlFor="name">First Name</label>
           <input
             type="text"
-            id="name"
+            id="firstName"
             value={firstName}
             onChange={firstChangeHandler}
             onBlur={firstBlurHandler}
           />
           {firstHasError && <p>Please enter a valid First Name</p>}
         </div>
-        <div className="form-control">
+        <div className={lastClassName}>
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={lastChangeHandler}
+            onBlur={lastBlurHandler}
+          />
+          {lastHasError && <p>Please enter a valid Last Name</p>}
         </div>
       </div>
-      <div className="form-control">
-        <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+      <div className={emailClassName}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+          />
+          {emailHasError && <p>Please enter a valid Email</p>}
       </div>
       <div className="form-actions">
-        <button disabled={formError}>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
--- 12-Forms/src/hooks/use-new.js
+++ 12-Forms/src/hooks/use-new.js
@@ -1,5 +1,33 @@
-const useNew = () => {
+import {useState} from 'react';
 
+const useNew = (validateField) => {
+  const [inputValue, setInputValue] = useState("");
+  const [inputIsTouched, setInputIsTouched] = useState(false);
+
+  const inputIsValid = validateField(inputValue);
+  const hasError = (!inputIsValid && inputIsTouched);
+
+  const inputChangeHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setInputIsTouched(true);
+  };
+
+  const cleanupHandler = (event) => {
+    setInputIsTouched(false);
+    setInputValue("");
+  };
+
+  return {
+    inputValue,
+    hasError,
+    inputIsValid,
+    inputChangeHandler,
+    inputBlurHandler,
+    cleanupHandler
+  }
 };
 
 export default useNew;
