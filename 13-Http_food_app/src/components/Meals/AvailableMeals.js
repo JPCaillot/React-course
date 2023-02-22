c0e9acd7747c220e481f6eb35a4bce6e366e8f85
JPCaillot <joaopcaillot@gmail.com>
2023-02-22 17:30:46 -0300

    Error handling added

--- 13-Http_food_app/src/components/Meals/AvailableMeals.js
+++ 13-Http_food_app/src/components/Meals/AvailableMeals.js
@@ -6,10 +6,17 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // I could also set it to false and set true once the fetch begins if the fetching begins at a different event
+  const [httpError, setHttpError] = useState(null); //or not passing any data at all, this is more explicit
 
   useEffect(() => {
     const fetchMeals  = async () => { //way to work around not being able to return promises in useEffect - running this function later inside the hook
       const response = await fetch('https://react-http-325a1-default-rtdb.firebaseio.com/meals.json');
+      
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -24,12 +31,38 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
-    };  
+      setIsLoading(false);
+    }; 
     
-    fetchMeals();
+    // PROMISE-ONLY WAY OF HANDLING ERRORS INSIDE OF A PROMISE:
+    
+    // try { // this could also be put in a separate function to avoid the promise effect of fecthMeals(), instead of the whole catch method
+      fetchMeals().catch(error => { //then gets the result of a successful promise, catch handles errors coming up inside of the promise 
+        setIsLoading(false);
+        setHttpError(error.message); //the message can be better constructed, this is just a way to show how to throw errors
+      }); 
+    // } catch (error) {
+    //   setIsLoading(false);
+    //   setHttpError(error.message);
+    // }
   }, []);
 
-  //we'll work with dummy data cause we don't know how to utilize databases yet
+  if (isLoading) {
+    return ( //this will be returned instead of the following return
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
+
   const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
