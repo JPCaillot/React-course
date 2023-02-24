5c40aaf19545e70c1037eb6394ef39d4227bf05b
JPCaillot <joaopcaillot@gmail.com>
2023-02-24 17:32:02 -0300

    Exploring redux toolkit

--- 15-Redux_react/src/components/Counter.js
+++ 15-Redux_react/src/components/Counter.js
@@ -1,30 +1,43 @@
 // import { useStore } from 'react-redux'; // {connect} for class-based components
 import { useSelector, useDispatch } from 'react-redux'; //selects specific parts of the store already
+import {counterActions} from '../store/index';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter); //automatic subscription by react-redux, managed by redux
+  const show = useSelector(state => state.showCounter);
   //whenever data changes in the store, you always get the latest state
   //if this is unmounted from the DOM, it's also automatically unsubscribed
 
   const incrementHandler = () => {
-    dispatch({type: 'increment'});
+    // dispatch({type: 'increment'}); //without toolkit
+    dispatch(counterActions.increment());
   };
+
+  const increaseHandler = () => {
+    // dispatch({type: 'increase', amount: 5});
+    dispatch(counterActions.increase(10)); // { type: SOME_UNIQUE_IDENTIFIER, payload: 10 } if just a number is declared instead of an object, for example
+  }
 
   const decrementHandler = () => {
-    dispatch({type: 'decrement'});
+    // dispatch({type: 'decrement'});
+    dispatch(counterActions.decrement());
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    // dispatch({type: 'toggle'});
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
@@ -33,6 +46,12 @@ const Counter = () => {
 };
 
 export default Counter;
+
+//this example would be properly handled with useState, because the states managed here are local, so this is just a demo of how Redux would work
+
+//---------------------------------------------------
+
+//CLASS-BASED COMPONENT EQUIVALENT:
 
 // import {connect} from 'react-redux';
 // import { Component } from 'react';
--- 15-Redux_react/src/store/index.js
+++ 15-Redux_react/src/store/index.js
@@ -1,19 +1,95 @@
-import {createStore} from 'redux';
+// import { createStore } from "redux"; //configureStore in the toolkit makes merging easier
+import { createSlice, configureStore } from "@reduxjs/toolkit"; //createReducer could also be used, but this is more powerful
 // import redux from 'redux';
 // const redux = require("redux");
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1 };
-  }
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
-  }
+const initialCounterState = { counter: 0, showCounter: true };
 
-  return state;
+const counterSlice = createSlice({
+  //slice of our global state
+  name: "counter", //needed, value up to you
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++; //in toolkit this is allowed, cause the inner workings will identify this and clone the state with the changes in an immutable way
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      //the action is also in the other two, but they don't need it
+      state.counter = state.counter + action.payload; //will be defined automatically
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const initialAuthState = {
+  isAuthenticated: false,
 };
 
-const store = createStore(counterReducer); //redux.createStore if redux from redux is imported
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     // as there are many if statements we could use switch case here
+
+//     //THIS SHOULD NEVER BE DONE:
+//     // state.counter++;
+
+//     // return state;
+//     //NEVER CHANGE ORIGINAL STATE, ALWAYS OVERWRITE IT, SO IT DOESN'T LEAD TO BUGS OR UNPREDICTABLE BEHAVIOR
+
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter, //redux won't merge changes with existing state, so this is needed
+//     };
+//   }
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount, // dynamic reducer adding extra properties
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "toggle") {
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// const store = createStore(counterReducer); //redux.createStore if redux from redux is imported
+// const store = createStore(counterSlice.reducer); //not viable for many reducers
+
+const store = configureStore({
+  // reducer: counterSlice.reducer // for many reducers, this could be an object, uniting them in a single object
+  reducer: { 
+    counter: counterSlice.reducer, 
+    auth: authSlice.reducer 
+  },
+});
 
 // THIS WILL BE DONE BY THE ACTUAL COMPONENTS:
 // const counterSubscriber = () => {
@@ -24,5 +100,8 @@ const store = createStore(counterReducer); //redux.createStore if redux from red
 // store.subscribe(counterSubscriber);
 
 // store.dispatch({ type: "increment" });
+
+export const counterActions = counterSlice.actions; //toolkit creates those methods for us with .actions
+export const authActions = authSlice.actions;
 
 export default store; // this needs to be provided - what does 'provide' mean?
