713de1a1982d2d1e658cd8d8057d7f02b1892b8a
JPCaillot <joaopcaillot@gmail.com>
2023-03-09 17:04:48 -0300

    Finished animations/started context/redux review

--- 23-Animations/src/components/Modal/Modal.js
+++ 23-Animations/src/components/Modal/Modal.js
@@ -1,24 +1,33 @@
 import React from "react";
 
+import CSSTransition from "react-transition-group/CSSTransition";
+
 import "./Modal.css";
 
-const modal = (props) => {
-  const cssClasses = [
-    "Modal",
-    props.show === "entering"
-      ? "ModalOpen"
-      : props.show === "exiting"
-      ? "ModalClosed"
-      : null,
-  ];
+const animationTiming = {
+  enter: 400,
+  exit: 1000
+};
 
+const modal = (props) => {
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <CSSTransition 
+      mountOnEnter 
+      unmountOnExit 
+      in={props.show} 
+      timeout={animationTiming}
+      classNames={{ // or just a string that will append -enter -enter-active
+        enterActive: 'ModalOpen',
+        exitActive: 'ModalClosed', //could also have appear, appearActive
+      }}
+    >
+        <div className='Modal'>
+          <h1>A Modal</h1>
+          <button className="Button" onClick={props.closed}>
+            Dismiss
+          </button>
+        </div>
+    </CSSTransition>
   );
 };
 
